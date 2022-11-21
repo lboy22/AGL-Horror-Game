e8b577a4a10ad7d9c0fcc801bc7d38a3e7f60a63
lboy22 <44554960+lboy22@users.noreply.github.com>
2022-11-21 12:22:50 -0800

    Update
    Added collectibles
    Count of objects
    Fixed issues with tilemap
    Added layers and organized editor menu

--- Assets/scripts/CollectibleObject.cs
+++ Assets/scripts/CollectibleObject.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class CollectibleObject : MonoBehaviour
 {
+
+    [SerializeField] public int mapPiecesLeft = 5;
+    [SerializeField] private TMPro.TMP_Text mapPiecesLeftText;
     // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         
+        if (collision.gameObject.CompareTag("Collectible"))
+        {
+            
+            Destroy(collision.gameObject);
+            mapPiecesLeft--;
+            mapPiecesLeftText.text = "Map Pieces left: " + mapPiecesLeft;
+        }
+        gameState();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void gameState()
     {
-        
+        if(mapPiecesLeft == 0)
+        {
+            //Yet to be implemented
+            //SceneManager.LoadScene("Winning scene");
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
     }
 }
