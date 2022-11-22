70071dd3939f129b785d50fcc0eb6a578b892a96
lboy22 <44554960+lboy22@users.noreply.github.com>
2022-11-21 17:42:10 -0800

    Added Timer

--- Assets/scripts/Timer.cs
+++ Assets/scripts/Timer.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using TMPro;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    //Starting time for game.
+    [SerializeField] public float timer = 5f;
+ 
+    [SerializeField ] private TextMeshProUGUI seconds;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(timer > 0f)
+        {
+            timer -= Time.deltaTime;        
+        }
+
+        else
+        {
+            //Load up game over scene.
+            //SceneManager.LoadScene('');
+
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        UpdateTimerDisplay(timer);
+    }
+
+    private void UpdateTimerDisplay(float time)
+    {
+        seconds.text = string.Format("{0}", (int)time);
     }
 }
