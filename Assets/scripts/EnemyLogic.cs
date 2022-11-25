b9794dd81266ccb7ca6c96be95ef80b9c9ba7ecb
lboy22 <44554960+lboy22@users.noreply.github.com>
2022-11-24 17:12:02 -0800

    Added spawning enemy.
    The enemy (white box) spawns from 'random' locations in the map. The game chooses from the already determined points placed in the map.
    Enemy detection is still missing.

---
+++ Assets/scripts/EnemyLogic.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLogic : MonoBehaviour
+{
+    private int randSpawnPoint;
+    public Transform[] spawnPoints;
+    public GameObject objectPrefab;
+
+    GameObject a;
+
+    private void Start()
+    {
+        randSpawnPoint = Random.Range(0, spawnPoints.Length);
+        a = Instantiate(objectPrefab, spawnPoints[randSpawnPoint].position, transform.rotation);
+    }
+
+    void SpawnEnemy()
+    {
+        Destroy(a);
+        randSpawnPoint = Random.Range(0, spawnPoints.Length);
+        a = Instantiate(objectPrefab, spawnPoints[randSpawnPoint].position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SpawnEnemy();
+        }      
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+}
